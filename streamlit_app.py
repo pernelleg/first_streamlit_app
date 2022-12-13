01620f36ef4cf4db5f5278dba67d0d87c2c85232
pernelleg <116500481+pernelleg@users.noreply.github.com>
2022-12-13 12:42:28 +0000

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My parents healthy dinner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
