5daf5fc21d624cb69016afbced29609a3b18ebd9
pernelleg <116500481+pernelleg@users.noreply.github.com>
2022-12-14 13:48:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ def get_fruit_load_list():
 #add a button
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list():
+  my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
